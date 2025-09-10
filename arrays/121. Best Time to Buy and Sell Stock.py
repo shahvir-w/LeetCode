22284170e9eb54c122389a50a6445dde5db0d376
Shahvir Wahab <shahvir05@icloud.com>
2025-09-09 23:17:06 -0400

    yerr

--- arrays/121. Best Time to Buy and Sell Stock.py
+++ arrays/121. Best Time to Buy and Sell Stock.py
@@ -1,18 +1,20 @@
 class Solution(object):
+    class Solution(object):
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-
         maxProfit = 0
-        buyPrice = prices[0]
+        currentLowestStock = 0
 
-        for price in prices[1:]:
-            if price < buyPrice:
-                buyPrice = price
+        for i in range(len(prices)):
+            if prices[i] <= prices[currentLowestStock]:
+                currentLowestStock = i
+            profit = prices[i] - prices[currentLowestStock]
 
-            maxProfit = max(maxProfit, price - buyPrice)
+            if profit > maxProfit:
+                maxProfit = profit
 
-        return maxProfit 
+        return maxProfit
         
